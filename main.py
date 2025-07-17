1c757c59270c131a50770af24ea602950c257396
bryanoke25 <bryan.oke25@gmail.com>
2025-07-17 18:56:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 
 haar_file = "haarcascade_frontalface_default.xml"
@@ -15,8 +16,23 @@ if not os.path.isdir(path):
 
 webcam = cv2.VideoCapture(0)
 face_cascade =cv2.CascadeClassifier(haar_file)
-
-
 count = 1
-
 while count <= 30:
+    read, img = webcam.read()
+    if not read:
+        print("could not capture image")
+        break
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+
+    if len(faces)> 0:
+        for x,y, w,h in faces:
+            cv2.rectangle(img, (x,y ), (x+w, y+h), (255, 200, 255), 2)
+            face= gray[y:y+h, x:x+w]
+            face_resize = cv2.resize(face, (width, height)) 
+            cv2.imwrite(os.path.join (path, f"{count}.png"), face_resize)
+            count += 1
+
+
+
+
